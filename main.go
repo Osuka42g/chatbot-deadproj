14ccb139f7705f2dd2212c77313b0017c11a471d
Osuka42g <osuka42@gmail.com>
2017-11-04 15:23:44 +0700

    [root] Add basic rules

---
+++ main.go
@@ -0,0 +1,11 @@
+// This file will contain a simple orchestation service for managing messaging, middleware and google-vision services.
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("This is a work in progress. Maybe you can start looking into inner directories.")
+}
